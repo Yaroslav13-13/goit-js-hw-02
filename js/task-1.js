ba22f66f57b757958f899e0388fa24f69c737de6
Yaroslav13-13 <frilanserwork13@gmail.com>
2025-05-18 17:34:56 +0300

    commit_hw_js_02

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,11 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits){
-
+    const totalPrice = quantity * pricePerDroid;
+    if (totalPrice > customerCredits) {
+        return "Insufficient funds!";
+    }
+    else {
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+    }
 }
 
 
